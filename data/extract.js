2b0e12b7e0c148c8f398ff52e33dd3db34472bed
warren488 <warren.s.488@gmail.com>
2022-09-12 06:42:56 -0400

    all new files needed for experiments

---
+++ data/extract.js
@@ -0,0 +1,114 @@
+const fs = require('fs')
+const filePath = './data/kepler/notts8000/'
+const filePaths = ['./data/kepler/notts8000/', './data/kepler/london8000/', './data/kepler/bbn8000/', './data/kepler/bbcentral8000/', './data/kepler/bbse8000/', './data/kepler/bbcap8000/']
+const fileNames = ['roads.wkt', 'tracks.wkt']
+// const file = filePath + fileName;
+
+function CSVtoWKT(filePath, fileName) {
+const file = filePath + fileName;
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    let file = data;
+    let lines = file.split('\n');
+    let headings = lines.shift().split(',');
+    const tracks = [];
+    const roads = [];
+    const tracksCSV = [headings.join(',')];
+    const roadsCSV = [headings.join(',')];
+
+    console.log(headings.indexOf("highway"))
+    for (let i = 0; i < lines.length; i++) {
+      // the first item has many commas in it so we need to separate by using quotes first
+      const splitLine = lines[i].split('"')
+      // the rest are separated by commas
+      const restofline = splitLine[2]?.split(',');
+      // first element will be an empty space
+      if (restofline && restofline[1] === "track") {
+        tracks.push(splitLine[1]);
+        tracksCSV.push(lines[i])
+      } else {
+        roads.push(splitLine[1]);
+        roadsCSV.push(lines[i])
+      }
+    }
+    // lines.unshift(headings)
+    if (roads.length > 0) {
+      fs.writeFile(filePath + 'roads.wkt', roads.join('\n'), err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        //file written successfully
+      })
+    }
+    if (tracks.length > 0) {
+      fs.writeFile(filePath + 'tracks.wkt', tracks.join('\n'), err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        //file written successfully
+      })
+    }
+    // this section below splits the csv to 2 separate csv files for roads and tracks
+    // if (roadsCSV.length > 1) {
+    //   fs.writeFile(filePath + 'roads.csv', roadsCSV.join('\n'), err => {
+    //     if (err) {
+    //       console.error(err)
+    //       return
+    //     }
+    //     //file written successfully
+    //   })
+    // }
+    // if (tracksCSV.length > 1) {
+    //   fs.writeFile(filePath + 'tracks.csv', tracksCSV.join('\n'), err => {
+    //     if (err) {
+    //       console.error(err)
+    //       return
+    //     }
+    //     //file written successfully
+    //   })
+    // }
+  });
+
+}
+
+function WKTtoCSV(pathName, fileName) {
+  fs.readFile(pathName + fileName, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    const CSVLines = ["WKT,"];
+
+    let file = data;
+
+    let lines = file.split('\n');
+    console.log(lines);
+    for (const line of lines) {
+      if (line.length > 0) {
+        CSVLines.push(`"${line}",`);
+      }
+    }
+    if (CSVLines.length > 1) {
+      fs.writeFile(pathName + fileName + '.csv', CSVLines.join('\n'), err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        //file written successfully
+      })
+    }
+  });
+}
+
+CSVtoWKT('./data/kepler/routes_container/', 'roads.csv')
+
+// filePaths.forEach(filePath => {
+//   fileNames.forEach(fileName => {
+//     WKTtoCSV(filePath, fileName);
+//   })
+// })
---
+++ generateData.bat
@@ -0,0 +1,66 @@
+@REM The commands below are those that have been used to generate the various reports
+
+@REM bluetooth range experiment
+.\one.bat -b 66 .\mod_def.txt .\new_settings\barbados_buses.txt .\new_settings\rng.txt .\new_settings\varying_bt_range.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\barbados_north_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_range.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\barbados_se_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_range.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\barbados_cen_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_range.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\barbados_cap_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_range.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\london_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_range.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\nottingham_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_range.txt
+
+@REM buffer sizze experiment
+.\one.bat -b 66 .\mod_def.txt .\new_settings\barbados_buses.txt .\new_settings\rng.txt .\new_settings\varying_buffer_size.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\barbados_north_settings.txt .\new_settings\rng.txt .\new_settings\varying_buffer_size.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\barbados_se_settings.txt .\new_settings\rng.txt .\new_settings\varying_buffer_size.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\barbados_cen_settings.txt .\new_settings\rng.txt .\new_settings\varying_buffer_size.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\barbados_cap_settings.txt .\new_settings\rng.txt .\new_settings\varying_buffer_size.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\london_settings.txt .\new_settings\rng.txt .\new_settings\varying_buffer_size.txt
+.\one.bat -b 66 .\mod_def.txt .\new_settings\nottingham_settings.txt .\new_settings\rng.txt .\new_settings\varying_buffer_size.txt
+
+@REM bluetooth speed experiment
+.\one.bat -b 30 .\mod_def.txt .\new_settings\barbados_buses.txt .\new_settings\rng.txt .\new_settings\varying_bt_speed.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\barbados_north_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_speed.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\barbados_se_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_speed.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\barbados_cen_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_speed.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\barbados_cap_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_speed.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\london_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_speed.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\nottingham_settings.txt .\new_settings\rng.txt .\new_settings\varying_bt_speed.txt
+
+@REM prophet time unit experiment
+.\one.bat -b 24 .\mod_def.txt .\new_settings\nottingham_settings.txt .\new_settings\rng.txt .\new_settings\varying_proph_time.txt
+.\one.bat -b 24 .\mod_def.txt .\new_settings\barbados_north_settings.txt .\new_settings\rng.txt .\new_settings\varying_proph_time.txt
+.\one.bat -b 24 .\mod_def.txt .\new_settings\barbados_se_settings.txt .\new_settings\rng.txt .\new_settings\varying_proph_time.txt
+.\one.bat -b 24 .\mod_def.txt .\new_settings\barbados_cen_settings.txt .\new_settings\rng.txt .\new_settings\varying_proph_time.txt
+.\one.bat -b 24 .\mod_def.txt .\new_settings\barbados_cap_settings.txt .\new_settings\rng.txt .\new_settings\varying_proph_time.txt
+.\one.bat -b 24 .\mod_def.txt .\new_settings\london_settings.txt .\new_settings\rng.txt .\new_settings\varying_proph_time.txt
+.\one.bat -b 24 .\mod_def.txt .\new_settings\barbados_buses.txt .\new_settings\rng.txt .\new_settings\varying_proph_time.txt
+
+@REM spray and wait number of copies experiment
+.\one.bat -b 42 .\mod_def.txt .\new_settings\nottingham_settings.txt .\new_settings\rng.txt .\new_settings\varying_saw_copies.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\barbados_north_settings.txt .\new_settings\rng.txt .\new_settings\varying_saw_copies.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\barbados_se_settings.txt .\new_settings\rng.txt .\new_settings\varying_saw_copies.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\barbados_cen_settings.txt .\new_settings\rng.txt .\new_settings\varying_saw_copies.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\barbados_cap_settings.txt .\new_settings\rng.txt .\new_settings\varying_saw_copies.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\london_settings.txt .\new_settings\rng.txt .\new_settings\varying_saw_copies.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\barbados_buses.txt .\new_settings\rng.txt .\new_settings\varying_saw_copies.txt
+
+@REM message frequency experiment
+.\one.bat -b 30 .\mod_def.txt .\new_settings\nottingham_settings.txt .\new_settings\rng.txt .\new_settings\varying_message_frequency.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\barbados_north_settings.txt .\new_settings\rng.txt .\new_settings\varying_message_frequency.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\barbados_se_settings.txt .\new_settings\rng.txt .\new_settings\varying_message_frequency.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\barbados_cen_settings.txt .\new_settings\rng.txt .\new_settings\varying_message_frequency.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\barbados_cap_settings.txt .\new_settings\rng.txt .\new_settings\varying_message_frequency.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\london_settings.txt .\new_settings\rng.txt .\new_settings\varying_message_frequency.txt
+.\one.bat -b 30 .\mod_def.txt .\new_settings\barbados_buses.txt .\new_settings\rng.txt .\new_settings\varying_message_frequency.txt
+
+@REM node density experiment
+.\one.bat -b 42 .\mod_def.txt .\new_settings\nottingham_settings.txt .\new_settings\rng.txt .\new_settings\varying_nodes.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\barbados_north_settings.txt .\new_settings\rng.txt .\new_settings\varying_nodes.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\barbados_se_settings.txt .\new_settings\rng.txt .\new_settings\varying_nodes.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\barbados_cen_settings.txt .\new_settings\rng.txt .\new_settings\varying_nodes.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\barbados_cap_settings.txt .\new_settings\rng.txt .\new_settings\varying_nodes.txt
+.\one.bat -b 42 .\mod_def.txt .\new_settings\london_settings.txt .\new_settings\rng.txt .\new_settings\varying_nodes.txt
+
+@REM node density for buses only experiment
+.\one.bat -b 36 .\mod_def.txt .\new_settings\barbados_buses.txt .\new_settings\rng.txt .\new_settings\varying_bus_nodes.txt
